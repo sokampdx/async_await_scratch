25d67cfcec8085e88ae3143a8eb9fd7e963b5833
Anthony <7993225+sokampdx@users.noreply.github.com>
2026-06-11 18:03:49 -0700

    printing all 1000s

--- async_await_scratch/Program.cs
+++ async_await_scratch/Program.cs
@@ -2,7 +2,7 @@ for (int i = 0; i < 1000; i++)
 {
     ThreadPool.QueueUserWorkItem(delegate
     {
-    Console.WriteLine(i);
+        Console.WriteLine(i); // print all 1000s
         Thread.Sleep(1000);
     });
     
