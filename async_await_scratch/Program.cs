20a5f9e04a757da26ef5bc19aa8bbe3eecd9780d
Anthony <7993225+sokampdx@users.noreply.github.com>
2026-06-11 18:20:55 -0700

    created MyThreadPool

--- async_await_scratch/Program.cs
+++ async_await_scratch/Program.cs
@@ -1,12 +1,36 @@
+using System.Collections.Concurrent;
+
 for (int i = 0; i < 1000; i++)
 {
     int localVal = i;
-    ThreadPool.QueueUserWorkItem(delegate
+    MyThreadPool.QueueUserWorkItem(() =>
     {
-        Console.WriteLine(localVal); // print all 1000s
+        Console.WriteLine(localVal);
         Thread.Sleep(1000);
     });
     
 }
 
 Console.ReadLine();
+
+static class MyThreadPool
+{
+    private static readonly BlockingCollection<Action> s_workItems = new();
+    public static void QueueUserWorkItem(Action action) => s_workItems.Add(action);
+
+    static MyThreadPool()
+    {
+        for (int i = 0; i < Environment.ProcessorCount; i++)
+        {
+            new Thread(() =>
+            {
+                while (true)
+                    {
+                        Action workItem = s_workItems.Take();
+                        workItem();
+                    }
+            })
+            { IsBackground = true }.Start();
+        }
+    }
+}
