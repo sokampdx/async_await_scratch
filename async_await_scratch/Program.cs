2f4e2eb54ff2bcfeeca7a49114ddcd00d4753812
Anthony <7993225+sokampdx@users.noreply.github.com>
2026-06-11 21:47:20 -0700

    completed  MyTask

--- async_await_scratch/Program.cs
+++ async_await_scratch/Program.cs
@@ -1,18 +1,122 @@
+// https://www.youtube.com/watch?v=R-z2Hv-7nxk
 using System.Collections.Concurrent;
+using System.Runtime.ExceptionServices;
 
 AsyncLocal<int> myVal = new();
-for (int i = 0; i < 1000; i++)
+List<MyTask> tasks = new();
+for (int i = 0; i < 50; i++)
 {
     myVal.Value = i;
-    MyThreadPool.QueueUserWorkItem(delegate
+    tasks.Add(MyTask.Run(delegate
     {
         Console.WriteLine(myVal.Value);
         Thread.Sleep(1000);
-    });
+    }));
     
 }
+foreach (var t in tasks) t.Wait();
 
-Console.ReadLine();
+class MyTask
+{
+    private bool _completed;
+    private Exception? _exception;
+    private Action? _continuation;
+    private ExecutionContext? _context;
+
+    public bool IsCompleted 
+    { 
+        get
+        {
+            lock(this) // don't do this! public object being lock
+            return _completed;
+        }
+    }
+
+    public void SetResult() => Complete(null);
+    public void SetException(Exception exception) { }
+
+    private void Complete(Exception? exception)
+    {
+        lock (this)
+        {
+            if (_completed) throw new InvalidOperationException("already completed");
+
+            _completed = true;
+            _exception = exception;
+            
+            if (_continuation is not null)
+            {
+                MyThreadPool.QueueUserWorkItem(delegate
+                {
+                    if (_context is null)
+                    {
+                        _continuation();
+                    } 
+                    else
+                    {
+                        ExecutionContext.Run(_context, (object? state) => ((Action) state!).Invoke(), _continuation);
+                    }
+                });
+            }
+        }
+    }
+    public void Wait()
+    {
+        ManualResetEventSlim? mres = null;
+
+        lock (this)
+        {
+            if (!_completed)
+            {
+                mres = new ManualResetEventSlim();
+                ContinueWith(mres.Set);
+            }
+        }
+        mres?.Wait();
+
+        if (_exception is not null)
+        {
+            ExceptionDispatchInfo.Throw(_exception);
+            // throw new AggregateException(_exception);
+        }
+    }
+
+    public void ContinueWith(Action action)
+    {
+        lock (this)
+        {
+            if (_completed)
+            {
+                MyThreadPool.QueueUserWorkItem(action);
+            }
+            else
+            {
+                _continuation = action;
+                _context = ExecutionContext.Capture();
+            }
+        }
+    }
+
+    public static MyTask Run(Action action)
+    {
+        MyTask t = new();
+
+        MyThreadPool.QueueUserWorkItem(() =>
+        {
+           try
+            {
+                action();
+            } 
+            catch (Exception e)
+            {
+                t.SetException(e);
+                return;
+            }
+            t.SetResult();
+        });
+        return t;
+    }
+}
 
 static class MyThreadPool
 {
@@ -34,7 +138,7 @@ static class MyThreadPool
                     }
                     else
                     {
-                        ExecutionContext.Run(context, state => ((Action) state!).Invoke(), workItem);
+                        ExecutionContext.Run(context, (object? state) => ((Action) state!).Invoke(), workItem);
                     }
                 }
             })
