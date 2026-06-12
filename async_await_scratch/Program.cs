99a2b321171f458d30fc1e5ea0c1e8b589a6d621
Anthony <7993225+sokampdx@users.noreply.github.com>
2026-06-11 18:05:42 -0700

    use local variable to allow thread retain correct number

--- async_await_scratch/Program.cs
+++ async_await_scratch/Program.cs
@@ -1,8 +1,9 @@
 for (int i = 0; i < 1000; i++)
 {
+    int localVal = i;
     ThreadPool.QueueUserWorkItem(delegate
     {
-        Console.WriteLine(i); // print all 1000s
+        Console.WriteLine(localVal); // print all 1000s
         Thread.Sleep(1000);
     });
     
